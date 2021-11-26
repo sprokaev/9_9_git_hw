d6d82143e8d3d619cc2c2df33fc69f6b4b994f98
Vova Oleynik <oleynik.96@gmail.com>
2021-11-27 01:05:28 +0200

    add test

--- src/test/java/gugu/qa/SimpleTest.java
+++ src/test/java/gugu/qa/SimpleTest.java
@@ -1,17 +1,28 @@
 package gugu.qa;
 
-import org.junit.jupiter.api.Assertions;
+import com.codeborne.selenide.*;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverConditions.url;
 
 class SimpleTest {
 
-    @Test
-    void firstTest() {
-        Assertions.assertTrue(true);
+    @BeforeAll
+    public static void setUp() {
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        Configuration.baseUrl = "https://github.com";
     }
 
     @Test
-    void secondTest() {
-        Assertions.assertTrue(true);
+    void shouldSearchInGitHub() {
+        Selenide.open("/");
+        $("[data-test-selector='nav-search-input']").click();
+        $("[data-test-selector='nav-search-input']").setValue("selenide").pressEnter();
+
+        webdriver().shouldHave(url("https://github.com/search?q=selenide"));
     }
+
+
 }
